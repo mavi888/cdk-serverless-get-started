e0c21eabf79db9a9b668b1be7b7b9880a6e44b9c
Villalba <lmv@a483e7ad5050.ant.amazon.com>
2020-07-20 21:18:43 +0300

    adding cdk stuff

--- lib/cdk-serverless-get-started-stack.ts
+++ lib/cdk-serverless-get-started-stack.ts
@@ -1,18 +1,39 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as apigw from "@aws-cdk/aws-apigateway";
 
 export class CdkServerlessGetStartedStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkServerlessGetStartedQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
+    //Dynamodb table definition
+    const table = new dynamodb.Table(this, "Hello", {
+      partitionKey: { name: "name", type: dynamodb.AttributeType.STRING },
     });
 
-    const topic = new sns.Topic(this, 'CdkServerlessGetStartedTopic');
+    // lambda function
+    const dynamoLambda = new lambda.Function(this, "DynamoLambdaHandler", {
+      runtime: lambda.Runtime.NODEJS_12_X,
+      code: lambda.Code.asset("functions"),
+      handler: "function.handler",
+      environment: {
+        HELLO_TABLE_NAME: table.tableName,
+      },
+    });
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    // permissions to lambda to dynamo table
+    table.grantReadWriteData(dynamoLambda);
+
+    // create the API Gateway with one method and path
+    const api = new apigw.RestApi(this, "hello-api");
+
+    api.root
+      .resourceForPath("hello")
+      .addMethod("GET", new apigw.LambdaIntegration(dynamoLambda));
+
+    new cdk.CfnOutput(this, "HTTP API URL", {
+      value: api.url ?? "Something went wrong with the deploy",
+    });
   }
 }
